4a164a3cf56b292cd55f1f93acdb6ed4feccd50f
Sean Shi <sshi11@stanford.edu>
2014-11-13 14:41:56 -0800

    pulled out functionality into other files

--- labeler.py
+++ labeler.py
@@ -2,6 +2,7 @@
 #########################################################################
 Created by: Sean Shi
 Created date: 11/12/14
+Modified data: 11/13/14
 -------------------------------------------------------------------------
 Script that allows user to choose summary sentences from a Yelp review
 and save the result in a dictionary with keys as reviewID and values
@@ -9,69 +10,12 @@ that are a list of sentence positions within the document that have been
 chosen to represent the review.
 -------------------------------------------------------------------------
 #########################################################################
-
 """
 
-import collections
 import json
-import re
 import sys
-
-'''
-    Breaks down the text of a Yelp review into the following constituents:
-    Sentences:  List of sentences that comprise the review.
-    Terms:      Dictionary of tokens in the review that are not stop words.
-    Keys are the tokens, values are the frequencies of the tokens.
-    '''
-class Review(object):
-    
-    # Function: Init
-    # --------------
-    # Generate a list of sentences based on splitting on punctuation
-    # that marks the end of a sentence. Initialize terms list
-    def __init__(self, reviewID, reviewText):
-        self.id = reviewID
-        self.sentences = [sentence.strip() \
-                          for sentence in re.split("[.?!]", reviewText) \
-                          if sentence.strip() != ""]
-        self.terms = collections.Counter()
-    
-    # Function: ProcessTerms
-    # ----------------------
-    # Builds a dictionary of tokens. Keys are the token strings.
-    # Values are the corresponding frequencies of the token strings.
-    def processTerms(self):
-        for sentence in self.sentences:
-            # Split sentence on whitespace to parse individual tokens
-            tokens = sentence.split();
-            for token in tokens:
-                if token not in stopWords:
-                    self.terms[token] += 1
-
-
-'''
-Pull out the first [numIDs] number of business IDs that are categorized
-as a Restaurant. There are a total of 14303 business that are categorized
-as such. Since it takes < 2 seconds to pull out all business IDs, all
-restaurant IDs are pulled out if no argument is provided
-'''
-def getRestaurantIDs(numIDs=15000):
-    # REPLACE WITH YOUR FILE-PATH TO yelp_academic_dataset_business.json HERE
-    businessJSON = r"yelp_academic_dataset_business.json"
-    handle = open(businessJSON, 'r');
-    count = 0
-    
-    restaurantIDs = []
-    for line in handle:
-        # Break from document parsing if input limit has reached
-        if count >= numIDs: break
-        business = json.loads(line)
-        if "Restaurants" in business["categories"]:
-            restaurantIDs.append(business["business_id"])
-            count += 1
-    handle.close()
-    return restaurantIDs
-
+import utils
+import YelpReview
 
 CHARACTER_MINIMUM = 500
 
@@ -90,10 +34,10 @@ if __name__ == '__main__':
     numReviews = endInd - startInd
 
     # Pulls in the first 50 restaurant IDs
-    restaurantIDs = getRestaurantIDs(50)
+    restaurantIDs = utils.getRestaurantIDs(50)
 
     # REPLACE WITH YOUR FILE-PATH TO yelp_academic_dataset_review.json HERE
-    reviewJSON = r"yelp_academic_dataset_review.json"
+    reviewJSON = r"../yelp_academic_dataset_review.json"
     handle = open(reviewJSON, 'r')
     output = open("Labeled_Reviews.json", 'w')
 
@@ -110,7 +54,7 @@ if __name__ == '__main__':
         if review["business_id"] in restaurantIDs:
             if len(review["text"]) > CHARACTER_MINIMUM:
                 # Generate a Review object for current review
-                r = Review(review["review_id"], review["text"])
+                r = YelpReview.Review(review["review_id"], review["text"])
                 
                 # Output relevant review contents
                 print "[Review %d]:" % (count+1)
@@ -131,5 +75,3 @@ if __name__ == '__main__':
 
     handle.close()
     json.dump(reviewDict, output, indent=4)
-        #for key, value in reviewDict.items():
-#print key, value
