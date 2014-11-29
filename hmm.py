a450e4d6a8724c6f43c853aa9dda48c8bdba43cf
Nitin <nkapania@gmail.com>
2014-11-29 11:45:45 -0800

    about to test MLE estimates

--- hmm.py
+++ hmm.py
@@ -10,7 +10,34 @@ Requires numpy package to be installed.
 """
 
 import utils
+import YelpReview
 import numpy as np
+
+
+class MultivariableNormals(object):
+    def __init__(self, numStates, numFeatures):
+        self.mu = []
+        self.sigma = []
+        self.counts = []
+        for i in range(numStates):
+            self.counts.append(0)
+            self.mu.append(np.asmatrix(np.zeros( (numFeatures, 1) ) ) )
+            self.sigma.append(np.asmatrix(np.zeros( (numFeatures, 1) ) ) )
+
+    def updateMu(self, state, phi):
+        phiM = np.asmatrix(phi).T
+        self.mu[state] = (self.mu[state]*self.counts[state] + phiM)/(self.counts[state] + 1)
+        self.counts[state] += 1
+
+    def updateSigma(self, state, phi):
+        temp = np.asmatrix(phi).T - self.mu[state]
+        cov = temp * temp.T 
+        self.sigma[state] = self.sigma[state] + cov
+
+    def normalizeSigma(self):
+        for state in range( len(self.counts) ):
+            self.sigma[state] = self.sigma[state] / self.counts[state]
+            
 
 #come up with state transition matrix from labeled data, as well
 #as p distribution of initial sentences, output in a tuple of (M, p)
@@ -18,25 +45,49 @@ import numpy as np
 #M is a (2N + 1) x (2N + 1) Markov transition matrix, where N is number of summary sentences in HMM
 #model, p is a (2*N + 1) distribution.
 
-def getStateTransitions(N = 6):
+def getMaxLikelihoodEstimates(N = 6, numFeatures = 4):
+
+    #initialize estimates for parameters b, p, and M
+    b = MultivariableNormals(N, numFeatures) #initialize collection of multivariable functions                   
     p = np.zeros((1, 2*N+1))
     M = np.zeros((2*N+1, 2*N+1))
     
     reviews = utils.loadLabeledReviews("Labeled_Reviews.json")
+
+    #first pass - transition matrix M, probability distribution p, feature means.
     for reviewID in reviews.keys():
         numSentences = reviews[reviewID].pop(0)
         isSummary = getSummaryFunc(reviews[reviewID])
+        review = utils.getReview(reviewID)
+        assert numSentences == len(review.sentences)  # sanity check that labeled review is the same as the sentence in the review object
 
         #get initial state and increment p array        
         state = 1 if isSummary(0) else 0
         p[0][state] += 1
 
         for i in range(1, numSentences):
+            b.updateMu(state, review.sentences[i].phi)
             newState = getSucc(state, isSummary(i), N)
             M[state, newState] += 1
             state = newState
 
-    return(normalize(M), normalize(p))    
+    #second pass - feature covariance matrices
+    for reviewID in reviews.keys():
+        isSummary = getSummaryFunc(reviews[reviewID])
+        review = utils.getReview(reviewID)
+
+        state = 1 if isSummary(0) else 0
+        for i in range(1, len(review.sentences)):
+            b.updateSigma(state, review.sentences[i].phi)
+            state = getSucc(state, isSummary(i), N)
+
+    #normalize 
+    p = normalize(p)
+    M = normalize(M)
+    b.normalizeSigma()
+        
+    return(M, p, b) 
+  
 
 #get successor state given current state, whether next sentence is summary or not, and number of summary sentences
 def getSucc(state, isSummary, N):
@@ -60,3 +111,7 @@ def normalize(A):
     for i in range(0, numRows): #iterate through each row
         A[i] = A[i]/sum(A[i])
     return A
+
+M, p, b = getMaxLikelihoodEstimates(3)
+print M
+print p
